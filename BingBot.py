4b04f514b97c16552fb8851dc6ea834897a9f212
itsb1ng <itsb1ng611@gmail.com>
2022-03-30 12:26:12 -0400

    Massive Update
    * Added Embed mode for messages
    * Nitro Sniper
    * Changed time of DM Webhook from GMT +2 to EST
    * Prepared for Valorant API handling with .env

--- BingBot.py
+++ BingBot.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import discord_self_embed
 import requests
 import os
 import shutil
@@ -8,10 +9,13 @@ import time
 import json
 import asyncio
 from discord import Webhook, RequestsWebhookAdapter, Embed
-import datetime
+from datetime import datetime
 import subprocess
 import math
 import sys
+from dotenv import load_dotenv
+import pytz
+from pytz import timezone
 
 """"
 MAKING AN ATTEMPT TO ADD OR REMOVE FROM THE CODE MAY RESULT IN THE BREAKING OF THE SCRIPT. IF YOU HAVE ANY FURTHER QUESTIONS
@@ -20,28 +24,32 @@ PLEASE CONTACT ME VIA DISCORD: https://discord.gg/Nh6wgyfAPR.
 GITHUB REPOSITORY: https://github.com/itsb1ng/BingBot-Self-Bot
 """
 
+load_dotenv()
+
 with open("config.json", "r") as f:
     config = json.load(f)
 
-DISCORD_TOKEN = config['discord_token']
-PREFIX = config['prefix']
-DM_WEBHOOK = config['webhook']
-API_KEY = config['api_key']
-STATUS = config['discord_status']
+DISCORD_TOKEN = config[0]['discord_token']
+PREFIX = config[0]['prefix']
+DM_WEBHOOK = config[0]['dm_webhook']
+API_KEY = config[0]['api_key']
+STATUS = config[0]['discord_status']
+NITRO_WEBHOOK = config[0]["nitro_webhook"]
+EMBED_MODE = config[0]['embed_mode']
 
 global width
 width = os.get_terminal_size().columns
 def startup(setting):
     print()
     print()
-    if setting == "standard" or setting == "":
+    if setting.lower() == "standard" or setting == "":
         print("██████╗ ██╗███╗   ██╗ ██████╗ ██████╗  ██████╗ ████████╗".center(width))
         print("██╔══██╗██║████╗  ██║██╔════╝ ██╔══██╗██╔═══██╗╚══██╔══╝".center(width))
         print("██████╔╝██║██╔██╗ ██║██║  ███╗██████╔╝██║   ██║   ██║   ".center(width))
         print("██╔══██╗██║██║╚██╗██║██║   ██║██╔══██╗██║   ██║   ██║   ".center(width))
         print("██████╔╝██║██║ ╚████║╚██████╔╝██████╔╝╚██████╔╝   ██║   ".center(width))
         print("╚═════╝ ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═════╝  ╚═════╝    ╚═╝   ".center(width))
-    elif setting == "3D":
+    elif setting.lower() == "3d":
         print(r"________  ___  ________   ________  ________  ________  _________    ".center(width))
         print(r"|\   __  \|\  \|\   ___  \|\   ____\|\   __  \|\   __  \|\___   ___\ ".center(width))
         print(r"\ \  \|\ /\ \  \ \  \\ \  \ \  \___|\ \  \|\ /\ \  \|\  \|___ \  \_| ".center(width))
@@ -49,7 +57,7 @@ def startup(setting):
         print(r"  \ \  \|\  \ \  \ \  \\ \  \ \  \|\  \ \  \|\  \ \  \\\  \   \ \  \ ".center(width))
         print(r"   \ \_______\ \__\ \__\\ \__\ \_______\ \_______\ \_______\   \ \__\ ".center(width))
         print(r"    \|_______|\|__|\|__| \|__|\|_______|\|_______|\|_______|    \|__|".center(width))
-    elif setting == "pointed":
+    elif setting.lower() == "pointed":
         print(r"▀█████████▄   ▄█  ███▄▄▄▄      ▄██████▄  ▀█████████▄   ▄██████▄      ███     ".center(width))
         print(r"  ███    ███ ███  ███▀▀▀██▄   ███    ███   ███    ███ ███    ███ ▀█████████▄ ".center(width))
         print(r"  ███    ███ ███▌ ███   ███   ███    █▀    ███    ███ ███    ███    ▀███▀▀██ ".center(width))
@@ -58,7 +66,7 @@ def startup(setting):
         print(r"  ███    ██▄ ███  ███   ███   ███    ███   ███    ██▄ ███    ███     ███     ".center(width))
         print(r"  ███    ███ ███  ███   ███   ███    ███   ███    ███ ███    ███     ███     ".center(width))
         print(r"▄█████████▀  █▀    ▀█   █▀    ████████▀  ▄█████████▀   ▀██████▀     ▄████▀   ".center(width))
-    elif setting == "money":
+    elif setting.lower() == "money":
         print(r"$$$$$$$\  $$$$$$\ $$\   $$\  $$$$$$\  $$$$$$$\   $$$$$$\ $$$$$$$$\ ".center(width))
         print(r"$$  __$$\ \_$$  _|$$$\  $$ |$$  __$$\ $$  __$$\ $$  __$$\\__$$  __|".center(width))
         print(r"$$ |  $$ |  $$ |  $$$$\ $$ |$$ /  \__|$$ |  $$ |$$ /  $$ |  $$ |   ".center(width))
@@ -67,7 +75,7 @@ def startup(setting):
         print(r"$$ |  $$ |  $$ |  $$ |\$$$ |$$ |  $$ |$$ |  $$ |$$ |  $$ |  $$ |   ".center(width))
         print(r"$$$$$$$  |$$$$$$\ $$ | \$$ |\$$$$$$  |$$$$$$$  | $$$$$$  |  $$ |   ".center(width))
         print(r"\_______/ \______|\__|  \__| \______/ \_______/  \______/   \__|   ".center(width))
-    elif setting == "blocks":
+    elif setting.lower() == "blocks":
         print(r" .----------------.  .----------------.  .-----------------. .----------------.  .----------------.  .----------------.  .----------------. ".center(width))
         print(r"| .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |".center(width))
         print(r"| |   ______     | || |     _____    | || | ____  _____  | || |    ______    | || |   ______     | || |     ____     | || |  _________   | |".center(width))
@@ -79,14 +87,14 @@ def startup(setting):
         print(r"| |              | || |              | || |              | || |              | || |              | || |              | || |              | |".center(width))
         print(r"| '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |".center(width))
         print(r" '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------' ".center(width))
-    elif setting == "graffiti":
+    elif setting.lower() == "graffiti":
         print(r"__________.___ _______    __________________ ___________________".center(width))
         print(r"\______   \   |\      \  /  _____/\______   \\_____  \__    ___/".center(width))
         print(r" |    |  _/   |/   |   \/   \  ___ |    |  _/ /   |   \|    |   ".center(width))
         print(r" |    |   \   /    |    \    \_\  \|    |   \/    |    \    |   ".center(width))
         print(r" |______  /___\____|__  /\______  /|______  /\_______  /____|   ".center(width))
         print(r"        \/            \/        \/        \/         \/         ".center(width))
-    elif setting == "slant":
+    elif setting.lower() == "slant":
         print(r" ______     __     __   __     ______     ______     ______     ______  ".center(width))
         print(r'/\  == \   /\ \   /\ "-.\ \   /\  ___\   /\  == \   /\  __ \   /\__  _\ '.center(width))
         print(r"\ \  __<   \ \ \  \ \ \-.  \  \ \ \__ \  \ \  __<   \ \ \/\ \  \/_/\ \/ ".center(width))
@@ -107,6 +115,8 @@ def song(song):
         playsound(r"sounds\dmsound.mp3")
     elif song == "failedlogin":
         playsound(r"sounds\failedlogin.mp3")
+    elif song == ("nitro"):
+        playsound(r"sounds\nitronotification.mp3")
 
 def getInfo(call):
     r = requests.get(call)
@@ -118,9 +128,9 @@ bing = commands.Bot(command_prefix=PREFIX, self_bot=True)
 async def on_connect():
     print(f"Logged in as: {bing.user}\n")
     song("start")
-    if STATUS == "dnd":
+    if STATUS.lower() == "dnd":
         await bing.change_presence(status=discord.Status.dnd)
-    elif STATUS == "idle":
+    elif STATUS.lower() == "idle":
         await bing.change_presence(status=discord.Status.idle)
     else:
         await bing.change_presence(status=discord.Status.online)
@@ -139,6 +149,30 @@ async def on_message(message):
                     await bing.invoke(ctx)
                 except:
                     pass
+    if (message.author.id != bing.user.id) and ("https://discord.gift/" in message.content):
+        if (config[1]['nitro'] == "on"):
+            try:
+                content = message.content.split(" ")
+                for item in content:
+                    if "https://discord.gift/" in item:
+                        GIFT_LINK = item
+                song("nitro")
+                print(f"\nNitro Sniped: {message.author}\n")
+                if NITRO_WEBHOOK != "":
+                    try:
+                        webhook = Webhook.from_url(NITRO_WEBHOOK, adapter=RequestsWebhookAdapter())
+                        embed = discord.Embed(title="Nitro Gift", color=0xC98FFC)
+                        embed.add_field(name="User:",value=message.author,inline=False)
+                        embed.add_field(name="Link:",value=GIFT_LINK,inline=False)
+                        MSG_LINK = message.jump_url
+                        embed.add_field(name="Go To Message:",value=f"[Message Link]({MSG_LINK})",inline=False)
+                        embed.set_thumbnail(url="https://static.roundme.com/upload/user/d30750eda6c30bba9295ad629961420555c05496.png")
+                        embed.set_footer(text="BingBot Self-Bot", icon_url="https://i.imgur.com/sKwqOXE.png")
+                        webhook.send(embed=embed)
+                    except:
+                        pass
+            except:
+                pass
     if (message.author.id != bing.user.id) and (message.channel.type is discord.ChannelType.private):
         if DM_WEBHOOK != "":
             try:
@@ -146,11 +180,10 @@ async def on_message(message):
                 embed = discord.Embed(title="Received DM", color=0xC98FFC)
                 embed.add_field(name="User:",value=message.author,inline=False)
                 embed.add_field(name="User ID:",value=message.author.id,inline=False)
-                if int(message.created_at.strftime("%H")) > 12:
-                    hour = int(message.created_at.strftime("%H")) - 10
-                else: 
-                    hour = message.created_at.strftime("%H")
-                embed.add_field(name="Received:",value=message.created_at.strftime(f"%A, %B %d %Y @ {hour}:%M %p GMT+2"),inline=False)
+                est = timezone('EST')
+                time= datetime.now(est)
+                hour = int(time.strftime("%H")) + 1
+                embed.add_field(name="Received:",value=time.strftime(f"%A, %B %d %Y @ {hour}:%M %p EST"),inline=False)
                 embed.set_thumbnail(url="https://i.imgur.com/sKwqOXE.png")
                 embed.set_footer(text="BingBot Self-Bot", icon_url="https://i.imgur.com/sKwqOXE.png")
                 song("dm")
@@ -158,6 +191,20 @@ async def on_message(message):
                 webhook.send(embed=embed)
             except:
                 pass
+
+@bing.command()
+async def commands(ctx):
+    with open ("commands.json", "r") as f:
+        data = json.load(f)
+    commands_list = list(data.keys())
+    commands_str = "\n".join(commands_list)
+    if EMBED_MODE.lower() == "on":
+        embed = discord_self_embed.Embed("Available Commands:",description=commands_str,colour="C98FFC",url="https://github.com/itsb1ng/BingBot-Self-Bot")
+        embed.set_author(ctx.author.display_name)
+        url = embed.generate_url(hide_url=True)
+        await ctx.send(url)
+    else:
+        await ctx.send(f"```\n\tAvailable Commands:\n{commands_str}\n```")
 
 @bing.command()
 async def pornhub(ctx, firstword, secondword):
@@ -191,7 +238,12 @@ async def selfpurge(ctx, num):
                 await asyncio.sleep(2)
             except:
                 pass
-    await ctx.send(f"`You have deleted {messagesdeleted} messages`")
+    if EMBED_MODE.lower() == "on":
+        embed = discord_self_embed.Embed(f"You have deleted {messagesdeleted} messages",colour="C98FFC",url="https://github.com/itsb1ng/BingBot-Self-Bot")
+        url = embed.generate_url(hide_url=True)
+        await ctx.send(url)
+    else:
+        await ctx.send(f"`You have deleted {messagesdeleted} messages`")
 
 @bing.command()
 async def hypixelstats(ctx, name):
@@ -209,9 +261,19 @@ async def hypixelstats(ctx, name):
                 rank = rank + "+"
         
         level = ((math.sqrt(data['player']['networkExp'] + 15312.5) - (125/math.sqrt(2))) / 25 * math.sqrt(2)) / 2
-        await ctx.send("```\n\t\t{}'s Hypixel Statistics:\nHypixel Rank: {}\nKarma: {:,}\nNetwork XP: {:,.2f}\nHypixel Level: {:.2f}\n```\nhttps://crafatar.com/renders/body/{}".format(name, rank, data['player']['karma'], data['player']['networkExp'], level, uuid))
+        if EMBED_MODE.lower() == "on":
+            embed = discord_self_embed.Embed(f"{name}'s Hypixel Statistics",description="Hypixel Rank: {}\nKarma: {:,}\nNetwork XP: {:,.2f}\nHypixel Level: {:.2f}".format(rank, data['player']['karma'], data['player']['networkExp'], level),colour="C98FFC",url="https://github.com/itsb1ng/BingBot-Self-Bot")
+            url = embed.generate_url(hide_url=True)
+            await ctx.send(f"{url}\nhttps://crafatar.com/renders/body/{uuid}")
+        else:
+            await ctx.send("```\n\t\t{}'s Hypixel Statistics:\nHypixel Rank: {}\nKarma: {:,}\nNetwork XP: {:,.2f}\nHypixel Level: {:.2f}\n```\nhttps://crafatar.com/renders/body/{}".format(name, rank, data['player']['karma'], data['player']['networkExp'], level, uuid))
     except:
-        await ctx.send("```\nError:\nInvalid IGN\n```")
+        if EMBED_MODE.lower() == "on":
+            embed = discord_self_embed.Embed(f"Error: Invalid IGN or API Key",colour="C98FFC",url="https://github.com/itsb1ng/BingBot-Self-Bot")
+            url = embed.generate_url(hide_url=True)
+            await ctx.send(url)
+        else:
+            await ctx.send("```\nError:\nInvalid IGN\n```")
 
 @bing.command()
 async def skills(ctx, name):
@@ -303,9 +365,19 @@ async def skills(ctx, name):
                 except:
                     return 0
             
-            await ctx.send("```\n\t\t{}'s Skyblock Skills:\nActive Profile: {}\nSkill Average: {:,.1f}\nFarming: {:.0f}\nMining: {:.0f}\nCombat: {:.0f}\nForaging: {:.0f}\nFishing: {:.0f}\nEnchanting: {:.0f}\nAlchemy: {:.0f}\nTaming: {:.0f}\n```".format(name,profile_name,skill_average(),farming(),mining(),combat(),foraging(),fishing(),enchanting(),alchemy(),taming()))
+            if EMBED_MODE.lower() == "on":
+                embed = discord_self_embed.Embed(f"{name}'s Skyblock Statistics",description="Active Profile: {}\nSkill Average: {:,.1f}\nFarming: {:.0f}\nMining: {:.0f}\nCombat: {:.0f}\nForaging: {:.0f}\nFishing: {:.0f}\nEnchanting: {:.0f}\nAlchemy: {:.0f}\nTaming: {:.0f}".format(profile_name,skill_average(),farming(),mining(),combat(),foraging(),fishing(),enchanting(),alchemy(),taming()),colour="C98FFC",url="https://github.com/itsb1ng/BingBot-Self-Bot")
+                url = embed.generate_url(hide_url=True)
+                await ctx.send(f"{url}")
+            else:
+                await ctx.send("```\n\t\t{}'s Skyblock Skills:\nActive Profile: {}\nSkill Average: {:,.1f}\nFarming: {:.0f}\nMining: {:.0f}\nCombat: {:.0f}\nForaging: {:.0f}\nFishing: {:.0f}\nEnchanting: {:.0f}\nAlchemy: {:.0f}\nTaming: {:.0f}\n```".format(name,profile_name,skill_average(),farming(),mining(),combat(),foraging(),fishing(),enchanting(),alchemy(),taming()))
         except:
-            await ctx.send("```\nError:\nInvalid IGN\n```")
+            if EMBED_MODE.lower() == "on":
+                embed = discord_self_embed.Embed(f"Error: Invalid IGN or API Key",colour="C98FFC",url="https://github.com/itsb1ng/BingBot-Self-Bot")
+                url = embed.generate_url(hide_url=True)
+                await ctx.send(url)
+            else:
+                await ctx.send("```\nError:\nInvalid IGN\n```")
     else:
         await ctx.send("`Invalid API Key`")
 
@@ -353,9 +425,19 @@ async def weight(ctx, name):
             def total_weight():
                 return overflow() + weight()
 
-            await ctx.send("```\n\t\t{}'s Skyblock Weight:\nActive Profile: {}\nWeight: {:,.2f}\nOverflow Weight: {:,.2f}\nTotal Weight: {:,.2f}\n```".format(name,profile_name,weight(),overflow(),total_weight()))
+            if EMBED_MODE.lower() == "on":
+                embed = discord_self_embed.Embed(f"{name}'s Skyblock Statistics",description="Active Profile: {}\nWeight: {:,.2f}\nOverflow Weight: {:,.2f}\nTotal Weight: {:,.2f}".format(profile_name,weight(),overflow(),total_weight()),colour="C98FFC",url="https://github.com/itsb1ng/BingBot-Self-Bot")
+                url = embed.generate_url(hide_url=True)
+                await ctx.send(f"{url}")
+            else:
+                await ctx.send("```\n\t\t{}'s Skyblock Weight:\nActive Profile: {}\nWeight: {:,.2f}\nOverflow Weight: {:,.2f}\nTotal Weight: {:,.2f}\n```".format(name,profile_name,weight(),overflow(),total_weight()))
         except:
-            await ctx.send("```\nError:\nInvalid IGN\n```")
+            if EMBED_MODE.lower() == "on":
+                embed = discord_self_embed.Embed(f"Error: Invalid IGN or API Key",colour="C98FFC",url="https://github.com/itsb1ng/BingBot-Self-Bot")
+                url = embed.generate_url(hide_url=True)
+                await ctx.send(url)
+            else:
+                await ctx.send("```\nError:\nInvalid IGN\n```")
     else:
         await ctx.send("`Invalid API Key`")
 
@@ -472,13 +554,23 @@ async def slayer(ctx, name):
             else:
                 ENDER_LVL = "0"
 
-            await ctx.send("```\n\t\t{}'s Slayer XP and Levels\nActive profile: {}\n\nRevenant Horror: Level {}\nSlayer XP {:,.0f}\n\nTarantula Broodfather: Level {}\nSlayer XP {:,.0f}\n\nSven Packmaster: Level {}\nSlayer XP {:,.0f}\n\nVoidgloom Seraph: Level {}\nSlayer XP {:,.0f}\n```".format(name, profile_name, REV_LVL, REV_XP, SPIDER_LVL, SPIDER_XP, SVEN_LVL, SVEN_XP, ENDER_LVL, ENDER_XP))
+            if EMBED_MODE.lower() == "on":
+                embed = discord_self_embed.Embed(f"{name}'s Skyblock Statistics",description="Active profile: {}\n\nRevenant Horror: Level {}\nSlayer XP {:,.0f}\n\nTarantula Broodfather: Level {}\nSlayer XP {:,.0f}\n\nSven Packmaster: Level {}\nSlayer XP {:,.0f}\n\nVoidgloom Seraph: Level {}\nSlayer XP {:,.0f}".format(profile_name, REV_LVL, REV_XP, SPIDER_LVL, SPIDER_XP, SVEN_LVL, SVEN_XP, ENDER_LVL, ENDER_XP),colour="C98FFC",url="https://github.com/itsb1ng/BingBot-Self-Bot")
+                url = embed.generate_url(hide_url=True)
+                await ctx.send(f"{url}")
+            else:
+                await ctx.send("```\n\t\t{}'s Slayer XP and Levels\nActive profile: {}\n\nRevenant Horror: Level {}\nSlayer XP {:,.0f}\n\nTarantula Broodfather: Level {}\nSlayer XP {:,.0f}\n\nSven Packmaster: Level {}\nSlayer XP {:,.0f}\n\nVoidgloom Seraph: Level {}\nSlayer XP {:,.0f}\n```".format(name, profile_name, REV_LVL, REV_XP, SPIDER_LVL, SPIDER_XP, SVEN_LVL, SVEN_XP, ENDER_LVL, ENDER_XP))
         except:
-            await ctx.send("```\nError:\nInvalid IGN\n```")
+            if EMBED_MODE.lower() == "on":
+                embed = discord_self_embed.Embed(f"Error: Invalid IGN or API Key",colour="C98FFC",url="https://github.com/itsb1ng/BingBot-Self-Bot")
+                url = embed.generate_url(hide_url=True)
+                await ctx.send(url)
+            else:
+                await ctx.send("```\nError:\nInvalid IGN\n```")
     else:
         await ctx.send("`Invalid API Key`")
 
-startup(config['startup'])
+startup(config[0]['startup'])
 try:
     bing.run(DISCORD_TOKEN)
 except:
--- README.md
+++ README.md
@@ -11,7 +11,12 @@ BingBot is a Self-Bot designed and coded by bing#0001. It offers some configurat
 
 # Config
 * discord_token = Refers to your Discord Token and the program will not be able to run without it
+* embed_mode = Whether or not to send your output as an embed
 * prefix = The prefix that is required for every command to invoke properly
-* webhook = The webhook where you would like to display when you receive a Direct Message from someone
+* dm_webhook = The webhook where you would like to display when you receive a Direct Message from someone
+* nitro_webhook = Webhook to receive nitro notifications
+* api_key = Hypixel api key to execute Hypixel related commands
 * discord_status = What you wish your status to be (Default is online). Available options: online (Online), dnd (Do Not Disturb), idle (Idle)
 * startup = The style of font you wish the starting header text to be. Available options: standard, 3D, pointed, money, blocks, graffiti, slant
+
+* nitro = The ability to receive console and audio notifications when someone posts a Nitro Gift link
